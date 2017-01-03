fb3ef6ecb365b490f2ca6ab98995c55e6fd6c4e7
Jiahao Li <lijiahao008@gmail.com>
2017-01-03 17:49:37 -0500

    wrapped

--- chess/cursor.rb
+++ chess/cursor.rb
@@ -101,7 +101,7 @@ class Cursor
   end
 
   def update_pos(diff)
-    pos = [cursor_pos[0] + diff[0], cursor_pos[1] + diff[1]]
+    pos = [(cursor_pos[0] + diff[0]) % 8, (cursor_pos[1] + diff[1]) % 8]
     self.cursor_pos = pos if board.in_bounds?(pos)
   end
 end
